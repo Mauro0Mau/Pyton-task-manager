80a2c9ce73018e90db1470be1349158536919ce8
Mauro0Mau <mauriciodasilvadesouza7270@gmail.com>
2025-03-08 13:47:59 -0300

    First commands in Python

---
+++ test.py
@@ -0,0 +1,37 @@
+nome = "Mauricio"
+
+print ("Esse cara é O cara: " + nome)
+
+x = 25
+y = 25
+z = 25
+
+print  (x + y)
+print  (x - z)
+print  (x * y)
+print  (x / z)
+
+a = 1 + 129
+d = - 130 
+c = a + d
+b = c + d
+
+print (b)
+        
+m = 18
+
+if m == 18:
+    print(b)
+else:
+    print(m)
+
+for i in range(1, 11):
+    print(i)
+    if i == 5:
+        break
+
+limite = 0
+
+while limite < 10:
+    print(limite)
+    limite +=1
